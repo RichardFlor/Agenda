5777ce3608d988a3e7af10337e21d2423a0d2433
RichardFlor <richardflor2015@gmail.com>
2022-06-29 17:30:45 -0300

    Conexão com o mysql usando JDBC

--- Agenda/src/main/java/models/DAO.java
+++ Agenda/src/main/java/models/DAO.java
@@ -1,5 +1,27 @@
 package models;
 
-public class DAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DAO {
+	/** Modulo de conexao **/
+	//Pârametros de conexão
+	private String driver = "com.mysql.cj.jdbc.Driver";
+	private String url = "jdbc:mysql://localhost:3306/agenda?useTimezone=true&serverTimezone=UTC";
+	private String user = "root";
+	private String password = "bcd127";
+	
+	//Método de conexão
+	private Connection conectar() {
+		Connection conexao = null;
+		try {
+			Class.forName(driver);
+			conexao = DriverManager.getConnection(url, user, password);
+			return conexao;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+	
 }
