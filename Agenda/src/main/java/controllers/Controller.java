bbb633949a024968b013428f48681590f6f19a9c
RichardFlor <richardflor2015@gmail.com>
2022-06-29 17:45:05 -0300

    Configurção da camada Controller

--- Agenda/src/main/java/controllers/Controller.java
+++ Agenda/src/main/java/controllers/Controller.java
@@ -8,31 +8,30 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import models.DAO;
 
-/**
- * Servlet implementation class Controller
- */
-@WebServlet(urlPatterns = {"/Controllers", "/main"})
+@WebServlet(urlPatterns = { "/Controllers", "/main" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	DAO dao = new DAO();
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Controller() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-		
-		
+	DAO dao = new DAO();
+
+	public Controller() {
+		super();
+
 	}
 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String action = request.getServletPath();
+		System.out.println(action);
+
+		if (action.equals("/main")) {
+			contatos(request, response);
+		}
+	}
+
+	// Listar Contatos
+	protected void contatos(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.sendRedirect("agenda.jsp");
+	}
 }
