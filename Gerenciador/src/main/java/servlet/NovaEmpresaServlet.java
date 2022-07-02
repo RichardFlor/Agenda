d6ea877087e6367c16e735ce406c5b7f9e9c27e1
RichardFlor <richardflor2015@gmail.com>
2022-07-01 22:41:17 -0300

    Cadastrando e Mostrando (banco ficticio(Classe Banco))

--- Gerenciador/src/main/java/servlet/NovaEmpresaServlet.java
+++ Gerenciador/src/main/java/servlet/NovaEmpresaServlet.java
@@ -8,18 +8,26 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+
 public class NovaEmpresaServlet extends HttpServlet {
-	
+
 	private static final long serialVersionUID = 1L;
 
-	
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		System.out.println("Cadastrando nova empresa!!");
-		
+
 		String nomeEmpresa = request.getParameter("nome");
 		
+		Empresa empresa = new Empresa();
+		empresa.setNome(nomeEmpresa);
+		
+		Banco banco = new Banco();
+		banco.adicionar(empresa);
+
 		PrintWriter out = response.getWriter();
-		out.println("<html><body>Empresa " +  nomeEmpresa + " cadastrada com sucesso!</body></html>");
+		out.println("<html><body>Empresa " + nomeEmpresa + " cadastrada com sucesso!</body></html>");
 	}
+	
 
 }
