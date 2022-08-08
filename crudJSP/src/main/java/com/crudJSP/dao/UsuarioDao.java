a6da9dc7984aac3ccce9b5a761774008fb9d64ae
RichardFlor <richardflor2015@gmail.com>
2022-08-08 20:50:58 -0300

    conexão com o banco de dados

--- crudJSP/src/main/java/com/crudJSP/dao/UsuarioDao.java
+++ crudJSP/src/main/java/com/crudJSP/dao/UsuarioDao.java
@@ -1,7 +1,50 @@
 package com.crudJSP.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.crudJSP.bean.Usuario;
+
 public class UsuarioDao {
 	
+	public static Connection getConnection() {
+		Connection con = null;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/crudjspjava","root", "bcd127");
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return con;
+	}
+	
+	public static List<Usuario> getAllUsuarios(){
+		List<Usuario> list = new ArrayList<Usuario>();
+		
+		try {
+			Connection con = getConnection();
+			PreparedStatement ps = con.prepareStatement("SELECT * FROM usuario");
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				Usuario usuario = new Usuario();
+				usuario.setId(rs.getInt("id"));
+				usuario.setNome(rs.getString("nome"));
+				usuario.setPassword(rs.getString("password"));
+				usuario.setSexo(rs.getString("sexo"));
+				usuario.setPais(rs.getString("pais"));
+				list.add(usuario);
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return list;
+	}
 	
 	
 }
